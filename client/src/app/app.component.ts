0b7a004bcd2017754768601608d2f06686951336
Andrey Vlasov <lion32211@yandex.ru>
2024-11-06 22:17:38 +0100

    Added IPagination interface for API response

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './modules/product';
+import { IPagination } from './modules/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'E-commerce';
-  products: any;
+  products: IProduct[];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('http://localhost:5153/api/products?pageSize=50').subscribe((response: any) => {
+    this.http.get('http://localhost:5153/api/products?pageSize=50').subscribe((response: IPagination) => {
       this.products = response.data;
       console.log(response);
     }, error => {
