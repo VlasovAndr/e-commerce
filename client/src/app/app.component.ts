b45affd3746efa5866987958dc1885a8e63ada00
Andrey Vlasov <lion32211@yandex.ru>
2024-11-13 20:15:32 +0100

    Organized project files and folders

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './modules/product';
-import { IPagination } from './modules/pagination';
+import { IProduct } from './shared/modules/product';
+import { IPagination } from './shared/modules/pagination';
 
 @Component({
   selector: 'app-root',
