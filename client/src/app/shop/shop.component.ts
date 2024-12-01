e3ca2c5033a00526b95294650d5b33b7eb270f2e
Andrey Vlasov <lion32211@yandex.ru>
2024-12-01 22:52:21 +0100

    Added display of brands and product types on the shopping page

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../shared/modules/product';
 import { ShopService } from './shop.service';
+import { IBrand } from '../shared/modules/brand';
+import { IType } from '../shared/modules/productType';
 
 @Component({
   selector: 'app-shop',
@@ -9,12 +11,34 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.getPtuducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getPtuducts() {
     this.shopService.getProducts().subscribe(response => {
       this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+  getBrands() {
+    this.shopService.getBrands().subscribe(response => {
+      this.brands = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+  getTypes() {
+    this.shopService.getTypes().subscribe(response => {
+      this.types = response;
     }, error => {
       console.log(error);
     });
