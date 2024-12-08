e4efebc3340abdd03f5201d70ff8ecead9e406fe
Andrey Vlasov <lion32211@yandex.ru>
2024-12-08 17:14:13 +0100

    Added pagination funcionality

---
+++ client/src/app/shared/modules/shopParams.ts
@@ -0,0 +1,7 @@
+export class ShoppParams {
+    brandId: number = 0;
+    typeId: number = 0;
+    sort = 'name';
+    pageNumber = 1;
+    pageSize = 6;
+}
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -4,6 +4,7 @@ import { IPagination } from '../shared/modules/pagination';
 import { IBrand } from '../shared/modules/brand';
 import { IType } from '../shared/modules/productType';
 import { map } from 'rxjs/operators';
+import { ShoppParams } from '../shared/modules/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +14,21 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(brandId?: number, typeId?: number, sort?: string) {
+  getProducts(shopParams: ShoppParams) {
 
     let params = new HttpParams();
 
-    if (brandId) {
-      params = params.append('brandId', brandId.toString())
+    if (shopParams.brandId != 0) {
+      params = params.append('brandId', shopParams.brandId.toString())
     }
 
-    if (typeId) {
-      params = params.append('typeId', typeId.toString())
+    if (shopParams.typeId != 0) {
+      params = params.append('typeId', shopParams.typeId.toString())
     }
 
-    if (sort) {
-      params = params.append('sort', sort)
-    }
+    params = params.append('sort', shopParams.sort)
+    params = params.append('pageIndex', shopParams.pageNumber.toString());
+    params = params.append('pageSize', shopParams.pageSize.toString());
 
     return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
       .pipe(
